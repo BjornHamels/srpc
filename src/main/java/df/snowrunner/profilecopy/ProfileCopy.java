2a49d672ec38c10a9b3b126c0ed9040b51fa0ae7
Bjorn Hamels <bjorn@hamels.nl>
2020-11-29 15:04:33 +0100

    Initial commit.

---
+++ src/main/java/df/snowrunner/profilecopy/ProfileCopy.java
@@ -0,0 +1,17 @@
+package df.snowrunner.profilecopy;
+
+/**
+ * Hello world!
+ */
+public final class ProfileCopy {
+    private ProfileCopy() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
